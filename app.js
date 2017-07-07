83e2644662573240a0d76d6ebf1ae5d91d3531ce
ScarDaddy2016 <john.r.scarborough@gmail.com>
2017-07-07 15:38:17 -0500

    Add files via upload
    updated this file from Ben's original

--- app.js
+++ app.js
@@ -1,7 +1,43 @@
-const express = require('express');
-const app = express();
+//const express = require('express');
+//const app = express();
+
+
+var util = require('util');
+var express = require('express');
+var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
+var app = express();
+
 const mustacheExpress = require('mustache-express');
 const routes = require('./routes/router.js')
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+// add validation methods to request
+app.use(expressValidator({
+    customValidators:{
+      // this is a custom validator to flag an
+      // error when a value is already in a given array
+      isNotIn: function(value, array){
+        var test = true;
+        for (var i = 0; i < array.length; i++) {
+          if (value.toUpperCase() == array[i]) {
+            test = false;
+          }
+        }
+
+        return test;
+      },
+      isDummy: function(value) {
+        return value;
+      }
+    }
+  }
+));
 
 app.use(express.static('public'))
 
